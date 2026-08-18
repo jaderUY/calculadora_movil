c6d54b4ee82a1ca4b186d4bdecbdc9995e237d73
jaderUY <jaderdavidaraujoarboleda@gmail.com>
2026-08-17 22:45:59 -0500

    Creacion del repositorio en git/github

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonToolbar, IonTitle, IonButton, IonGrid, IonRow, IonCol],
 })
+
 export class HomePage {
-  constructor() {}
-}
+  display: string = '0';
+  firstOperand: number | null = null;
+  operator: string | null = null;
+  waitingForSecondOperand: boolean = false;
+
+  // funcion para la logica de los numoros y decimales
+  appendNumber(num: string) {
+    if (this.waitingForSecondOperand) {
+      this.display = num;
+      this.waitingForSecondOperand = false;
+    } else {
+      this.display = this.display === '0' ? num : this.display + num;
+    }
+  }
+
+  // funcion para la logica del punto
+  appendDecimal() {
+    if (this.waitingForSecondOperand) {
+      this.display = '0';
+      this.waitingForSecondOperand = false;
+      return;
+    } else {
+      if (!this.display.includes('.')) this.display += '.';
+    }
+  }
+
+  // duncion para la logica de las operaciones básicas
+  setOperation(op: string) {
+    const inputValue = parseFloat(this.display);
+    if (this.firstOperand === null) {
+      this.firstOperand = inputValue;
+    } else if (this.operator) {
+      const result = this.calculateResult(this.firstOperand, inputValue, this.operator);
+      this.display = String(result);
+      this.firstOperand = result;
+    }
+    this.operator = op;
+    this.waitingForSecondOperand = true;
+  }
+} 
